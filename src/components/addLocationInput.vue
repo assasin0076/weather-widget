b4331cbf637625202f49239cce22b61afbe46c44
Victor <viktor.generalov@nlogic.ai>
2023-02-05 21:06:00 +0400

    fix: fixed search

--- src/components/addLocationInput.vue
+++ src/components/addLocationInput.vue
@@ -6,39 +6,44 @@ import type { TPlace } from "@/types/TPlace";
 import type { TLocation } from "@/types/TLocation";
 
 const places = ref<TPlace[]>([]);
+
+const computedPlaces = computed(() => {
+  return places.value;
+});
 const locationsStore = useLocationsStore();
 
-const searchValue = computed({
-  get: () => "",
-  set: (e: TPlace | string) => {
-    if (typeof e === "string") return;
-    const location: TLocation = {
-      name: e.city,
-      lat: e.latitude,
-      lon: e.longitude,
-      state: e.region,
-      country: e.countryCode,
-    };
-    locationsStore.addLocation(location);
-  },
-});
+function selectHandle(option: TPlace) {
+  const location: TLocation = {
+    name: option.city,
+    lat: option.latitude,
+    lon: option.longitude,
+    state: option.region,
+    country: option.countryCode,
+  };
+  locationsStore.addLocation(location);
+}
 let timeout: ReturnType<typeof setTimeout>;
 
-function inputHandle(e: any) {
+async function searchHandle(search: any, loading: any) {
+  loading(true);
   clearTimeout(timeout);
   timeout = setTimeout(async () => {
-    places.value = await getPlaces(e?.target?.value);
+    places.value = await getPlaces(search);
+    loading(false);
   }, 300);
 }
 </script>
 
 <template>
-  <v-select v-model="searchValue" @input="inputHandle" :options="places">
+  <v-select
+    @search="searchHandle"
+    @option:selected="selectHandle"
+    :options="computedPlaces"
+    :resetOnOptionsChange="true"
+    :filterable="false"
+  >
     <template #open-indicator="{ attributes }">
       <span v-bind="attributes"></span>
-    </template>
-    <template #no-options="{}">
-      <div></div>
     </template>
   </v-select>
 </template>
