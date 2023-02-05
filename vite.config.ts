5865d3f756d0ea058abcce821ad78d6a9f560128
Victor <viktor.generalov@nlogic.ai>
2023-02-05 11:54:11 +0400

    chore: fix the progress

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,30 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  build: {
+    lib: {
+      entry: "./src/weather-widget.js",
+      formats: ["es", "cjs"],
+      name: "weather-widget",
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+    },
+    sourcemap: true,
+    target: "esnext",
+    minify: false,
   },
 });
